a22dd113b31685e1f76a93ebc9c81450ab79ed40
aaallexus <alexei@falko.com.ua>
2019-07-01 13:21:03 +0300

    asdasd

--- test.py
+++ test.py
@@ -2,4 +2,4 @@
 import sys
 def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
-    return [b"Hello World-------!!!!!!_-----___-_--____-"] # python2
+    return [b"Hello World"] # python2
