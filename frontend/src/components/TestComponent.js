3ee01b00561201b8c4369688f62752d2cd1d37e5
aaallexus <alexei@falko.com.ua>
2020-01-13 11:24:09 +0200

    fix

--- frontend/src/components/TestComponent.js
+++ frontend/src/components/TestComponent.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+//    <Link to='/users'>Next</Link>
 export default function TestComponent(){
     return (<div>
                 <label>Hello</label>
-                <Link to='/users'>Next</Link>
             </div>)
 }
