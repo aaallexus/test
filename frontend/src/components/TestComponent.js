ca7aed3222e7a0d5d8cc6d55acb3b2801c08fb17
aaallexus <a.falko@naviwfm.com>
2022-01-05 16:23:11 +0200

    Commit 2

--- frontend/src/components/TestComponent.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-export default function TestComponent(){
-    return (<div>
-                <label>Hello</label>
-                <Link to='/users'>Next</Link>
-            </div>)
-}
--- rest/README.md
+++
@@ -1,2 +0,0 @@
-
-CXBI Rest Server
